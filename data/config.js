4448d34934d40db01b65b39ab275beea6742df85
literature <65801567+LiteraturePro@users.noreply.github.com>
2024-01-05 23:53:08 +0800

    Update config.js

--- data/config.js
+++ data/config.js
@@ -12,11 +12,11 @@ const baseConfig = {
 
 // 数据库配置 (注意：除数据库连接池大小外，以下配置项提及的内容需在安装前准备好并填入)
 const databaseConfig = {
-  host: "cdb-9f2p00jq.cd.tencentcdb.com", // 数据库链接地址
-  port: "10104", // 数据库连接端口
+  host: "146.56.153.99", // 数据库链接地址
+  port: "3306", // 数据库连接端口
   database: "bing", // 数据库名
-  user: "literature", // 数据库用户名
-  password: "yxl981204@", // 数据库密码
+  user: "root", // 数据库用户名
+  password: "yxl981204#", // 数据库密码
   connectionLimit: 100, // 数据库连接池大小
 };
 
